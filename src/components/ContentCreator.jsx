dae20059d50aa572caff30ecac600404a9b2bf64
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-08-13 15:55:14 -0400

    imageURL showing in EditCreator.jsx

--- src/components/ContentCreator.jsx
+++ src/components/ContentCreator.jsx
@@ -19,9 +19,6 @@ function ContentCreator({ creators, onDelete }) {
           <div className={`content-cover content-cover${(index % 3) + 1}`}>
             <h1 style={{ margin: "0.25em" }}>{creator.name}</h1>
             <h2 className="description-text" style={{ margin: "0.1em", marginBottom: "1.5em" }}>{creator.description}</h2>
-            <a href={creator.url} target="_blank" rel="noopener noreferrer">
-              {creator.url}
-            </a>
             <button onClick={() => navigate(`/view/${creator.id}`)}>View</button>
             <button onClick={() => navigate(`/edit/${creator.id}`)}>Edit</button>
             <button onClick={() => handleDelete(creator.id)}>Delete</button>
